ded50ffb4349e77fc1c7b069942a3848b086b9e7
ShawnSjl <sjlshawn@gmail.com>
2025-04-03 20:23:19 -0400

    disable overscroll

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,3 +5,13 @@
 <template>
   <RouterView />
 </template>
+
+<style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+  overscroll-behavior: none;
+}
+</style>
