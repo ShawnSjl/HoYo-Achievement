3d37865af538d51761d670defe3763b66c5ef91f
ShawnSjl <sjlshawn@gmail.com>
2025-04-04 14:36:41 -0400

    update user table

--- db/init/001_create_user_table.sql
+++ db/init/001_create_user_table.sql
@@ -2,5 +2,7 @@ USE HoYo_Achievement;
 CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(512) NOT NULL
+    password VARCHAR(512) NOT NULL,
+    role ENUM('admin', 'user') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
