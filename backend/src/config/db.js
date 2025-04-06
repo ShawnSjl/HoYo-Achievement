21a16e8a406b398cd932c7156062b3d9c5e309f9
ShawnSjl <sjlshawn@gmail.com>
2025-04-06 17:47:30 -0400

    frontend support docker file and new env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,6 +8,7 @@ function initDB() {
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_PORT,
     });
     return pool;
 }
