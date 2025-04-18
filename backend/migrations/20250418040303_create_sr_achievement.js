842f297eed65357c30fc909a3e5a32a2293e145f
ShawnSjl <sjlshawn@gmail.com>
2025-04-18 00:27:18 -0400

    create sr achievement database table and seed loader

---
+++ backend/migrations/20250418040303_create_sr_achievement.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.createTable('sr_achievement', function (table) {
+      table.integer('achievement_id').notNullable().primary();
+      table.string('class', 20).notNullable();
+      table.string('name', 128).notNullable();
+      table.string('description', 256).notNullable();
+      table.integer('reward_level').defaultTo(1);
+      table.string('game_version', 16).notNullable();
+      table.integer('hidden').defaultTo(0);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTable('sr_achievement');
+};
