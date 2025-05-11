43858297fadb24cf96b67469725256d39450db25
ShawnSjl <sjlshawn@gmail.com>
2025-05-11 15:34:41 -0400

    update announcement content

--- frontend/src/views/User/AnnouncementCard.vue
+++ frontend/src/views/User/AnnouncementCard.vue
@@ -7,13 +7,16 @@
     <el-card shadow="never" style="height: 360px">
       <template #header>
         <div slot="header">
-          网站公告
+          网站公告 版本：v1.1.0
         </div>
       </template>
 
       <div>
-        <p>网站版本：v1.1</p>
-        <p>网站基础功能已施工完成</p>
+        <b>新增功能</b>
+        <p>1. 使用hash实现内容定位，页面刷新时不会退回到第一个内容选项</p>
+        <b>修复</b>
+        <p>1. 修复在使用平板等低分辨率的场景下的布局错误</p>
+        <p>2. 优化移动端模式检测逻辑</p>
       </div>
     </el-card>
   </div>
